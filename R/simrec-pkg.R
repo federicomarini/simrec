eb0ae840c401a17eaf46460e1cee71cf784ed11b
Federico Marini <marinif@uni-mainz.de>
2019-01-18 16:26:10 +0100

    added docs for the package itself

---
+++ R/simrec-pkg.R
@@ -0,0 +1,20 @@
+#' simrec
+#' 
+#' Simulation of recurrent event data for non-constant baseline hazard
+#' (total-time model)
+#' 
+#' Simulation of recurrent event data for non-constant baseline hazard 
+#' in the total time model with risk-free intervalls and possibly a competing 
+#' event. Possibility to cut the data to an interim data set. Data can be plotted.
+#' 
+#' @importFrom graphics axis lines par plot
+#' @importFrom stats na.omit pnorm qnorm rbinom rgamma rnorm runif
+#' 
+#' @author
+#' Katharina Ingel, 2016-2017;
+#' 
+#' Maintainer: TBD \email{TBD@@uni-mainz.de}
+#' 
+#' @name simrec-pkg
+#' @docType package
+NULL
